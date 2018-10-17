d291f1d7f6813d90d40d5a0dc157937c776d9490
Henrik Blidh <henrik.blidh@swedwise.com>
2018-10-17 12:21:49 +0200

    Updated to use Framework 4.7.1 images

--- WAFCtest/Controllers/ValuesController.cs
+++ WAFCtest/Controllers/ValuesController.cs
@@ -2,42 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
+using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;
+using ControllerBase = Microsoft.AspNetCore.Mvc.ControllerBase;
 
 namespace WAFCtest.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
     public class ValuesController : ControllerBase
     {
         // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public string[] Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [Microsoft.AspNetCore.Mvc.HttpGet("{id}")]
+        public string Get(int id)
         {
             return "value";
         }
 
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        public void Post([Microsoft.AspNetCore.Mvc.FromBody] string value)
         {
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [Microsoft.AspNetCore.Mvc.HttpPut("{id}")]
+        public void Put(int id, [Microsoft.AspNetCore.Mvc.FromBody] string value)
         {
         }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
+        [Microsoft.AspNetCore.Mvc.HttpDelete("{id}")]
         public void Delete(int id)
         {
         }
--- WAFCtest/Startup.cs
+++ WAFCtest/Startup.cs
@@ -37,10 +37,10 @@ namespace WAFCtest
             }
             else
             {
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
